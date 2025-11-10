a1dd4e8532a7164ec91c9221432aa90139bffb54
LivinkumarStackly <livinkumar@thestackly.com>
2025-11-10 12:18:23 +0530

    Variables, datatypes and Typecasting

---
+++ VariablesDatatypeTypeCasting.py
@@ -0,0 +1,34 @@
+# a1=10  # creating a1. assigning 10
+# a1=50  # updating a1
+# print(a1)
+
+a=10
+b=10.78
+c="Hello"
+d="World"
+
+name="Vijay"
+age=51  # 51 --> "51"  --> str()
+
+# print(name+str(age))
+
+
+# print("His name is " +name+"."+" he is "+str(age)+" years")
+
+#His name is Vijay. He is 51 years old!
+
+# print(56)
+# print("Livin")
+
+# a1=input("Enter a word: ") 
+
+# print(a1)
+# print(a1)
+# print(a1)
+# print(a1)
+# print(a1)
+
+# print(4+"6")
+
+num=input("Enter a number: ") #"66"
+print(int(num)+10)

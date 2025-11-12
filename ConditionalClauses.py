92c92fc6258e74bae240b53867ad2f9bb02fecdf
LivinkumarStackly <livinkumar@thestackly.com>
2025-11-12 12:50:41 +0530

    Control Statements - Conditional Clause

---
+++ ConditionalClauses.py
@@ -0,0 +1,66 @@
+# age=int(input("Enter age: "))
+
+# if age>=18:
+#     print("You can vote") #age>=18
+# else:
+#     print("You cannot vote") #age<18
+
+# print("independent")
+# If 
+
+# a=10
+
+# if a<7:
+#     print("From If Block")
+# else:
+#     print("From Else Block")
+
+# print("Step3")
+# print("End")
+
+
+# a=10
+
+# if a!=10:
+#     print("From If Block")
+# elif a!=10:
+#     print("From Elif Block 1")
+# elif a==10:
+#     print("From Elif Block 2")
+# else:
+#     print("print From Else Block")
+
+
+# marks = int(input("Marks scored:   "))
+# if marks > 90:
+#       print ("O Grade")
+# elif marks>=81 and marks<=90:
+#       print ("A+ Grade")
+# elif marks <= 71:
+#    print ("A Grade")
+# elif marks <= 61:
+#    print ("B Grade")
+# elif marks <= 51:
+#    print ("C Grade")
+# else:
+#     print ("Fail")
+
+a=10
+
+# if a==10:
+#     if a>8:
+#         print("Hi")
+
+
+num=int(input("Enter Number: ")) #10
+
+if num>10:
+    if num>15:
+        print("very Large")
+    else:
+        print("Large")
+else:
+    if num<5:
+        print("very small")
+    else:
+        print("small")
--- Operators.py
+++ Operators.py
@@ -49,3 +49,7 @@ b=-3
 # print(3&4)
 # print(3|4)
 # print(5^7)
+
+# print(~8)
+# print(7<<3)
+print(17>>2)

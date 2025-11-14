ec11959b1b7d0797a45853a2eca2f15a671a593f
LivinkumarStackly <livinkumar@thestackly.com>
2025-11-14 13:22:33 +0530

    Control Statements

--- Loop.py
+++ Loop.py
@@ -43,5 +43,33 @@
 # .
 # 0
 
-for i in range(100,-1,-1):
-    print (i)
+# for i in range(100,-1,-1):
+#     print (i)
+
+# [0,1,2,3,.....9]
+
+# for i in range(10):
+#     if i==5:
+#         break
+#     print(i)
+#[0,1,2,3....49,50,51,.....99]
+
+# for i in range(100):
+#     if i==50:
+#         continue
+#     print("---------")
+#     print(i)
+#     print("----------")
+
+# for i in range(100):
+#     print(i)
+
+secret=33
+
+while True:
+    guessed_value=int(input("Guess the 2 digit number: "))
+    if guessed_value==secret:
+        print("You got it!")
+        break
+    else:
+        print("Try again!")

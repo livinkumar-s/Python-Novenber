c809eab4661293561dc6ce3ca6ace54fb7983626
LivinkumarStackly <livinkumar@thestackly.com>
2025-11-17 15:35:47 +0530

    Datastructures - List

---
+++ Datastructures.py
@@ -0,0 +1,36 @@
+# a="Hello guys I am from tamilnadu!"
+# print(len(a))
+
+# print(a[29])
+# print(a[6:10])  #6,7,8,9
+# print(a[-10:-5])  
+# print(a[-10:])  
+# print(a[:8])  
+
+# print(a[::-1])
+
+
+# List
+
+# a=[1,2,3,"Hello","Hi",3.14,False]
+# print(len(a))
+# print(a[5])
+# print(a[a[2]])
+
+
+# print(a[4])
+# a[4]=66
+
+# a.append("last")
+# a.extend("hello")
+# a.insert(4,100)
+a=[4,3,2,5,6,7,8,8,7] #0
+# a.remove(7)
+# a.pop(5)
+# a.pop()
+# print(a.index(8))
+# print(a.index(88))
+# print(a.count(80))
+a.sort()
+a.reverse()
+print(a)

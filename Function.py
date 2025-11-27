c94ccfb1bd4b61155d5f2b4e939b713291c13b50
LivinkumarStackly <livinkumar@thestackly.com>
2025-11-27 17:00:12 +0530

    Functions

---
+++ Function.py
@@ -0,0 +1,50 @@
+# def printStep():
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+
+# printStep()
+# printStep()
+# printStep()
+
+# def add():
+#     a=10
+#     b=10
+#     print(a+b)
+
+# add()
+
+
+# def add(a,b):
+#     print("A="+str(a))
+#     print("B="+str(b))
+
+# add("livin",55)
+# add(30,40)
+# add(b=30,a=40)
+
+# def name(n1,n2="hardhika"):
+#     print(n1+n2)
+
+# name("Good evening ","Anything")
+
+def findSum(*a): #kwargs
+    # ans=0
+    # for i in a:
+    #     ans=ans+i
+    print(sum(a))
+
+findSum(23,43,546,56,5443,43)
+
+# def dummy(a,b):
+#     print(a+b)
+
+# dummy(2,3)
+
+# t1=(1,2,3,4,5)
+
+
+
+# print(ans)
+
+# print(1)

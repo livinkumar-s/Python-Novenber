aa0314d4d241bf2cf721011bde8e0cacf2ea943f
LivinkumarStackly <livinkumar@thestackly.com>
2025-11-28 12:24:22 +0530

    String

---
+++ String.py
@@ -0,0 +1,53 @@
+# s="Hello"
+# s[0]="M"
+
+# inp 
+# palindrome
+# "pal"
+# "no pal"
+
+# string=input("Enter a string: ")
+# def palindrome(s):
+#     return s==s[::-1]
+# if palindrome(string):
+#     print("palindrome")
+# else:
+#     print("not a palindrome")
+
+# print("Hello"+"Hi")
+# print("Hello"*8) 
+
+# print("Hell" not in "Hello")
+
+
+# print(a.upper()) #"HELLO"
+# print(a.lower()) #"hello"
+# print(a.title()) #"hello"
+
+# print(a.find('lo'))
+# print(a.find('Code'))
+# print(a.count('ode'))
+
+# print("123e".isdigit())
+
+a="i would love to code"
+
+# print(a.replace("i","we"))
+
+# ['i w','uld l',]
+
+# print(a.split("o"))
+
+# lis1=["Hello","World","From","moon"]
+
+# print('---'.join(lis1))
+# b="        hello world        "
+# print(b)
+# print(b.strip())
+
+name="Vijay"
+age=51  
+
+print("His name is "+name+" and he is "+str(age)+" years old!")
+
+print(f"His name is {name} and he is {age} years old!")

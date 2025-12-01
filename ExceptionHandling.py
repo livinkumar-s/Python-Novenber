3385f2d4223413ba27e6d7df9b8377f53fffbc11
LivinkumarStackly <livinkumar@thestackly.com>
2025-12-01 12:08:32 +0530

    Exception Handling

---
+++ ExceptionHandling.py
@@ -0,0 +1,45 @@
+print("Step1")
+print("Step2")
+print("Step3")
+
+# if True:
+# print("Hello")
+
+
+# try:
+#     print(8/0)
+# except ZeroDivisionError:
+#     print("Invalid expression!")
+
+# print(8/0)
+
+# try:
+#     print(5/0)
+#     print("2"+2)
+# except TypeError:
+#     print("invalid print statement!")
+# except ZeroDivisionError:
+#     print("Invalid expression")
+
+
+# print(num1/num2)
+
+
+# try:
+#     num1=int(input("Enter number 1: "))
+#     num2=int(input("Enter number 2: "))
+#     print(num1/num2)
+# except ZeroDivisionError:
+#     print(f"The number {num1} cannot be divided by {num2}")
+# except ValueError:
+#     print("Invalid Input")
+# finally:
+#     print("Finished!")
+
+
+
+i=0
+
+while i<=10:
+    print(i)
+    i-=1
